c971df7b7b1e18ef6bef9894dba9e5029a5dcbf8
Billy <billyz404meter@gmail.com>
2018-08-16 14:00:03 -0700

    add palsquare

---
+++ 1.3/palsquare.cpp
@@ -0,0 +1,45 @@
+/*
+ID: NomNom
+PROG: palsquare
+LANG: C++
+*/
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+bool ispal(string s){
+    int i = 0,j = s.size();
+    j--;
+    while(s[i] == s[j]){
+        if(i == j || i == j-1) return true;
+        i++; j--;
+    }
+    return false;;
+}
+
+string convert(int isquare, int base){
+    string nums = "0123456789ABCDEFGHIJ";
+    string digits;
+    while(isquare != 0){
+        digits.insert(digits.begin(),nums[isquare%base]);
+        isquare /= base;
+    }
+    return digits;
+}
+
+int main(){
+    std::ifstream in("palsquare.in");
+    std::ofstream out("palsquare.out");
+    int base;
+    in >> base;
+
+    for(int i = 1; i <= 300; i++){
+        int isquare = i * i;
+        string digits = convert(isquare,base);
+        if(ispal(digits)){
+             out << convert(i, base) << ' ' << digits << '\n';;
+        }
+    }
+    return 0;
+}
