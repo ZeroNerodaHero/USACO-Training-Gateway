33908492f5ec75a4ab58c55248872e114c3740d2
Billy <billyz404meter@gmail.com>
2018-08-15 19:37:02 -0700

    add name

---
+++ 1.3/namenum.cpp
@@ -0,0 +1,50 @@
+/*
+ID: NomNom
+PROG: namenum
+LANG: C++
+*/
+#include <iostream>
+#include <map>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main(){
+    std::ifstream in("namenum.in");
+    std::ifstream fin("dict.txt");
+    std::ofstream out("namenum.out");
+
+    std::map<char,char> letter;
+    char a = 'A';
+    for(char i = 50; i < 57; i++){
+        for(int j = 0; j < 3; j++,a++){
+            if(a == 'Q'){
+                a++;
+            }
+            letter[a] = i;
+        }
+    }
+
+    std::string number;
+    in >> number;
+    int len = number.size();
+   
+    bool isempty = true;;
+     
+    for(int i = 0; i < 4617; i++){
+        std::string name;
+        fin >> name;
+        if(name.size() == len){
+            for(int j = 0; j < len; j++){
+                if(letter[name[j]] != number[j])
+                    break;
+                if(j == len-1){
+                    out << name << '\n';
+                    isempty = false;
+                }                 
+            }
+        }
+    }
+    if(isempty) out << "NONE" << '\n';   
+    return 0;
+}
