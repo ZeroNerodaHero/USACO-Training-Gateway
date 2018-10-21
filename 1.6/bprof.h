e254786461c0b32c6665265053a336cb624b6b62
Billy <billyz404meter@gmail.com>
2018-10-20 20:39:14 -0700

    dfs

---
+++ 1.6/bprof.h
@@ -0,0 +1,53 @@
+#ifndef _BPROF_HEADER_
+#define _BPROF_HEADER_
+
+#include <iostream>
+#include <time.h>
+
+#define PROFILE_START clock_t t1 = clock();
+
+#define PROFILE_STOP\
+    clock_t t2 = clock();\
+    long diff = (t2-t1)*1000000/CLOCKS_PER_SEC;\
+    cout <<"time:\t" <<diff << endl;
+
+#define PROFILE(x)\
+    do{\
+        clock_t t1, t2;\
+        t1 = clock();\
+        x;\
+        t2 = clock();\
+        long diff = (t2-t1)*1000000/CLOCKS_PER_SEC;\
+        cout <<"time:\t" <<diff << endl;\
+    }while(0)
+
+class singleton{
+ public:
+    static singleton &get(){
+        static singleton n;
+        return n;
+    }
+    void profile_start(){
+        t1 = clock();
+    }
+    void profile_stop(){
+        t2 = clock();
+        long diff1 = (t2-t1)*1000000/CLOCKS_PER_SEC;
+        long diff2 = diff1/1000;
+        std::cout << times++ << " takes " << diff1 << "us ("<<diff2<<" ms)\n";
+    }
+ private:
+    singleton() {}
+    clock_t t1, t2;
+    int times;
+};
+
+void profile_start(){
+    singleton::get().profile_start();
+}
+
+void profile_stop(){
+    singleton::get().profile_stop();
+}
+
+#endif
---
+++ 1.6/dfs.cpp
@@ -0,0 +1,82 @@
+/*
+ID: billy
+PROB: pprime
+LANG: C++11
+*/
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <fstream>
+#include <vector>
+    std::ifstream in("pprime.in");
+    std::ofstream out("pprime.out");
+using namespace std;
+int prime[3] = {5,7,11};
+int odd[4] = {1,3,7,9};
+//int ar[10000];
+vector<int> ar;
+int size = 0;
+int a,b;
+
+bool isprime(int n){
+    int y = sqrt(n);
+    for(int i = 3; i <= y; i++){
+        if((n%i)==0){
+            return false;
+        }
+    }
+    return true;
+}
+
+inline int pal(int buf[],int l, int even){
+    int j = l+1;
+    for(int i = even?l:l-1; i >= 0; i--){
+        buf[j++] = buf[i];
+    } 
+    int sum = 0;
+    for(int i = 0; i < j; i++){
+        sum = sum*10+buf[i];
+    }
+    return sum;
+}
+
+void dfs(int buf[], int l){
+    for(int i = 0; i < 10; i++){
+        buf[l] = i;
+        for(int j = 0; j < 2; j++){
+            int n = pal(buf,l,j);
+            if(n > b){
+                if(j) continue;
+                else return;
+            }
+            if(n < a) continue;
+            //if(isprime(n)) ar[size++] = n;
+            if(isprime(n)) ar.push_back(n);
+        }
+        dfs(buf,l+1);
+    }
+}
+
+int main(){
+    in >> a >> b;
+    std::cout << a << ' ' << b << std::endl;
+    for(int i = 0; i < 3 && prime[i] <= b; i++){
+        if(a > prime[i]) continue;
+        out << prime[i] << endl;
+    }
+    int buf[16];
+    if(b > 100){
+        for(int i = 0; i < 4; i++){
+            buf[0] = odd[i];
+            dfs(buf,1);
+        }
+    }
+    //sort(ar,ar+size);
+    sort(ar.begin(),ar.end());
+    //for(int i = 0; i < size; i++){
+    for(int i = 0; i < ar.size(); i++){
+        out << ar[i] << endl; 
+    }
+    return 0;
+}
+
